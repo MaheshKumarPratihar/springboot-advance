643033e229428da04713bce8f8fbf6493297cc42
MaheshKumarPratihar <maheshkumarpratihar@gmail.com>
2025-05-06 22:19:56 +0300

    Task_1

--- src/main/java/com/springboot/homework/HomeworkApplication.java
+++ src/main/java/com/springboot/homework/HomeworkApplication.java
@@ -1,13 +1,19 @@
 package com.springboot.homework;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeworkApplication {
+public class HomeworkApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args){
+		System.out.println("hello world");
 	}
 
 }
